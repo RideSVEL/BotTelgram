7e71146cd82511e7fa9c96e8101e4b3fc9e5512c
RideSVEL <sbkiking3@gmail.com>
2020-02-23 22:22:30 +0200

    Modify functional of bots.

--- src/main/java/model/Bot.java
+++ src/main/java/model/Bot.java
@@ -5,27 +5,44 @@ import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
-import static org.telegram.telegrambots.logging.BotLogger.log;
+import static sun.util.logging.LoggingSupport.log;
 
-
+//TODO safe migrate on LATEST version TELEGRAM API
 public class Bot extends TelegramLongPollingBot {
 
     boolean starting = false;
     String name = "";
 
-    private synchronized void sendMsg(Message message, String text, boolean reply) {
+    /**
+     * @param message get message parameters
+     * @param text    text your message
+     * @param reply   reply on message
+     * @param menu    show menu
+     */
+    private synchronized void sendMsg(Message message, String text, boolean reply, boolean menu) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId());
         if (reply) {
             sendMessage.setReplyToMessageId(message.getMessageId());
         }
+        if (menu) {
+            setButtons(sendMessage);
+        }
+
         sendMessage.setText(text);
         //System.out.println(message.getText());
         try {
@@ -35,42 +52,117 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+
+    /**
+     * @param update get updates messages from telegram server
+     */
     @Override
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
-
+        SendMessage sendMessage = new SendMessage();
         if (message != null && message.hasText()) {
 
             if (starting) {
                 name = message.getText();
-                sendMsg(message, "Привет, " + name + "!\nКак у тебя дела?", false);
+                sendMsg(message, "Привет, " + name + "!\nКак у тебя дела?", false,false);
                 starting = false;
             } else {
                 switch (message.getText()) {
                     case "/start":
-                        sendMsg(message, "Привет, как твое имя?", false);
-                        starting = true;
+                        sendMsg(message, "Выбери пункт из меню ниже:", false, true);
+                        setButtons(sendMessage);
+
                         break;
                     case "/help":
-                        sendMsg(message, "How are you today?", false);
+                        sendMsg(message, "How are you today?", false, false);
+                        break;
+                    case "Давай знакомиться":
+                        sendMsg(message, "Как вас зовут?", false, false);
+                        starting = true;
                         break;
                     case "/settings":
-                        sendMsg(message, "It is settings mod", false);
+                        sendMsg(message, "It is settings mod", false, false);
+                        setInline();
                         break;
                     default:
-                        sendMsg(message, "Mistake input", true);
+                        sendMsg(message, "Mistake input", true, false);
                         break;
                 }
             }
         }
     }
 
+// TODO make this method workable and next
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//        if (update.hasMessage()) {
+//            ThreadClass thread = new ThreadClass(update.getMessage());
+//        } else if (update.hasCallbackQuery()) {
+//            AnswerCallbackThread answerThread = new AnswerCallbackThread(update.getCallbackQuery());
+//        }
+//    }
+
+// TODO make this method workable and prev
+//    public synchronized void answerCallbackQuery(String callbackId, String message) {
+//        AnswerCallbackQuery answer = new AnswerCallbackQuery();
+//        answer.setCallbackQueryId(callbackId);
+//        answer.setText(message);
+//        answer.setShowAlert(true);
+//        try {
+//            answerCallbackQuery(answer);
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    /**
+     * @return bot`s name, make by registration
+     */
     public String getBotUsername() {
-        return "AbituraKHPI";
+        return "@EnrolleeKhpi_bot";
     }
 
+    /**
+     * @return bot token to connect the telegram server
+     */
     public String getBotToken() {
         return "909718704:AAF1O6VxafVioSkxD6bM1UU_gza0uH6MQlI";
+    }
+
+    /**
+     * @param sendMessage return message for send
+     *                    set keyboards
+     */
+    public synchronized void setButtons(SendMessage sendMessage) {
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        // создаем строки
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow firstRow = new KeyboardRow();
+        KeyboardRow secondRow = new KeyboardRow();
+
+        firstRow.add(new KeyboardButton("Давай знакомиться"));
+        secondRow.add(new KeyboardButton("Помощь"));
+
+        keyboardRows.add(firstRow);
+        keyboardRows.add(secondRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+
+    }
+
+
+    private void setInline() {
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+        List<InlineKeyboardButton> buttons1 = new ArrayList<>();
+        buttons1.add(new InlineKeyboardButton().setText("Кнопка").setCallbackData(String.valueOf(17)));
+        buttons.add(buttons1);
+
+        InlineKeyboardMarkup markupKeyboard = new InlineKeyboardMarkup();
+        markupKeyboard.setKeyboard(buttons);
     }
 
     public static void main(String[] args) {
