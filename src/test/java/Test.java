41ffd0e309ab84e249d70f674c4eeff4e19a6b64
Sergey <sbkiking3@gmail.com>
2020-05-04 19:08:00 +0300

    Add stickers. Change bugs on properties.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,16 +3,21 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.AmericanEnglish;
+import org.languagetool.language.Russian;
+import org.languagetool.rules.RuleMatch;
 import telegram.bot.config.BotConfig;
 import telegram.bot.entity.Country;
+import telegram.bot.model.FormStats;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 import java.util.logging.SimpleFormatter;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        Date date = new Date();
 //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 //        System.out.println(simpleDateFormat.format(date));
@@ -20,14 +25,28 @@ public class Test {
 //        cal.add(Calendar.DATE, -1);
 //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 //        System.out.println(simpleDateFormat.format(cal.getTime()));
-        String result = "{\"Country\":\"Ukraine\",\"CountryCode\":\"UA\",\"Province\":\"\",\"City\":\"\",\"CityCode\":\"\",\"Lat\":\"48.38\",\"Lon\":\"31.17\",\"Confirmed\":8125,\"Deaths\":201,\"Recovered\":782,\"Active\":7142,\"Date\":\"2020-04-25T00:00:00Z\"}";
-        HttpResponse<String> response = getResponse("ukraine", -1);
-        System.out.println(response.getBody());
-        String temp =  response.getBody();
+//        String result = "{\"Country\":\"Ukraine\",\"CountryCode\":\"UA\",\"Province\":\"\",\"City\":\"\",\"CityCode\":\"\",\"Lat\":\"48.38\",\"Lon\":\"31.17\",\"Confirmed\":8125,\"Deaths\":201,\"Recovered\":782,\"Active\":7142,\"Date\":\"2020-04-25T00:00:00Z\"}";
+//        HttpResponse<String> response = getResponse("ukraine", -1);
+//        System.out.println(response.getBody());
+//        String temp =  response.getBody();
 //        temp = temp.replaceAll("\\[", "");
 //        temp = temp.replaceAll("\\]", "");
 //        System.out.println(temp);
-        Country country = parseCountry(temp);
+//        Country country = parseCountry(temp);
+//        System.out.println(country);
+//        JLanguageTool lt = new JLanguageTool(new Russian());
+//
+//        List<RuleMatch> matches = lt.check("укроена лудшая страна");
+//        List<String> list = null;
+//        for (RuleMatch match : matches) {
+////            System.out.println("Potential error at characters " +
+////                    match.getFromPos() + "-" + match.getToPos() + ": " +
+////                    match.getMessage());
+////            System.out.println("Suggested correction(s): " +
+//                list = match.getSuggestedReplacements();
+//        }
+
+        Country country = FormStats.spellChecking("укроиНА");
         System.out.println(country);
     }
 
